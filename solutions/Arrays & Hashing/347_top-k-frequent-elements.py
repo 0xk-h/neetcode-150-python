3b5261612cd32d0468b34dabc80d29594c7d6e34
kishore399 <kishore.j21.01@gmail.com>
2025-11-16 23:20:58 +0530

    feat: add solution for Top K Frequent Elements

---
+++ solutions/Arrays & Hashing/347_top-k-frequent-elements.py
@@ -0,0 +1,27 @@
+from collections import Counter
+from typing import List
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        return sorted(set(nums), key=lambda x: -freq[x])[:k]
+    
+# Time Complexity: O(n log n)
+# Space Complexity: O(n)
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        
+        n = [(-freq[i], i) for i in set(nums)]
+        heapq.heapify(n)
+
+        res = [0] * k
+        for i in range(k):
+            res[i] = heapq.heappop(n)[1]
+
+        return res
+
+# Time Complexity: O(n + k log n)
+# Space Complexity: O(n)
