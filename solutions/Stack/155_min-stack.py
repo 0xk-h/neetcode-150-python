81daec00456c741168b7509ee82f7cd6d13e470f
kishore399 <kishore.j21.01@gmail.com>
2025-11-19 20:05:59 +0530

    feat: add efficient MinStack implementation

--- solutions/Stack/155_min-stack.py
+++ solutions/Stack/155_min-stack.py
@@ -2,6 +2,60 @@ class MinStack:
 
     def __init__(self):
         self.stk = []
+        self.m = float("inf")
+
+    def push(self, val: int) -> None:
+        if not self.stk:
+            self.stk.append(0)
+            self.m = val
+        else:
+            x = val - self.m
+            self.stk.append(x)
+            if x < 0:
+                self.m = val
+        
+
+    def pop(self) -> None:
+        val = self.stk.pop()
+        if val < 0:
+            self.m -= val
+
+    def top(self) -> int:
+        top = self.stk[-1]
+        if top > 0: 
+            return self.stk[-1] + self.m
+        else:
+            return self.m
+        
+
+    def getMin(self) -> int:
+        return self.m
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+#---------------------------------------------------------
+#           Time Complexity:
+#           push:    O(1)
+#           pop:     O(1)
+#           top:     O(1)
+#           getMin:  O(n)
+#           
+#           Space Complexity:
+#           O(1)
+#---------------------------------------------------------
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stk = []
         self.m = []
 
     def push(self, val: int) -> None:
@@ -23,7 +77,6 @@ class MinStack:
     def getMin(self) -> int:
         return self.m[-1]
         
-
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
