7fe12aa9f9b78ec4819e58614420bfc7a2a58f44
kishore399 <kishore.j21.01@gmail.com>
2025-11-19 17:39:51 +0530

    feat: implement MinStack data structure

---
+++ solutions/Stack/155_min-stack.py
@@ -0,0 +1,78 @@
+class MinStack:
+
+    def __init__(self):
+        self.stk = []
+        self.m = []
+
+    def push(self, val: int) -> None:
+        self.stk.append(val)
+        if not self.m or self.m[-1] >= val:
+            self.m.append(val)
+        
+
+    def pop(self) -> None:
+        val = self.stk.pop()
+        if self.m and self.m[-1] == val:
+            self.m.pop()
+        
+
+    def top(self) -> int:
+        return self.stk[-1]
+        
+
+    def getMin(self) -> int:
+        return self.m[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+#---------------------------------------------------------
+#           Time Complexity:
+#           push:    O(1)
+#           pop:     O(1)
+#           top:     O(1)
+#           getMin:  O(1)
+#           
+#           Space Complexity:
+#           O(n)
+#---------------------------------------------------------
+
+
+# Brute Force Solution
+class MinStack:
+
+    def __init__(self):
+        self.stk = []
+
+    def push(self, val: int) -> None:
+        self.stk.append(val)
+        
+
+    def pop(self) -> None:
+        self.stk.pop()
+        
+
+    def top(self) -> int:
+        return self.stk[-1]
+        
+
+    def getMin(self) -> int:
+        return min(self.stk)
+    
+#---------------------------------------------------------
+#           Time Complexity:
+#           push:    O(1)
+#           pop:     O(1)
+#           top:     O(1)
+#           getMin:  O(n)
+#           
+#           Space Complexity:
+#           O(n)
+#---------------------------------------------------------
