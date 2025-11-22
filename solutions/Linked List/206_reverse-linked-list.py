c6313e20ad27c25cd909f0563d03cbb20bef91d9
kishore399 <kishore.j21.01@gmail.com>
2025-11-22 16:01:12 +0530

    refactor: import ListNode from linkedlist

--- solutions/Linked List/206_reverse-linked-list.py
+++ solutions/Linked List/206_reverse-linked-list.py
@@ -1,11 +1,5 @@
 from typing import Optional
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+from linkedlist import ListNode
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
