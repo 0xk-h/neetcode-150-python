4657227260d6bed04cfd95495766c6e7d9811b61
kishore399 <kishore.j21.01@gmail.com>
2025-11-22 13:54:11 +0530

    feat: add iterative solution for reversing linked list

---
+++ solutions/Linked List/206_reverse-linked-list.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        return prev
+    
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        prev = head
+        curr = head.next
+        prev.next = None
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        return prev
+    
+# Time Complexity: O(n)
+# Space Complexity: O(1)
