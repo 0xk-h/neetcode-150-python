89ac323eec166de10697f59de94da968ec5d50a6
kishore399 <kishore.j21.01@gmail.com>
2025-11-23 12:54:53 +0530

    feat: implement Node class for linked list with random pointer

---
+++ solutions/Linked List/randomlist.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+    @staticmethod
+    def from_list(lst):
+        if not lst:
+            return None
+        
+        nodes = [Node(val) for val, _ in lst]
+        n = len(nodes)
+        
+        for i in range(n - 1):
+            nodes[i].next = nodes[i + 1]
+        
+        for i, (_, r_idx) in enumerate(lst):
+            if r_idx is not None:
+                nodes[i].random = nodes[r_idx]
+        
+        return nodes[0]
+    
+    
+    # Only shows the next pointer in the representation.
+    def __repr__(self):
+        return f"Node( { " -> ".join(str(v) for v in self) } )"
+    
+    # Only yeild the next pointer in the iteration.
+    def __iter__(self):
+        node = self
+        while node:
+            yield node.val
+            node = node.next
+
+    def __eq__(self, other):
+        def build_index_map(head):
+            idx_map = {}
+            node_list = []
+            node = head
+            idx = 0
+            while node:
+                idx_map[node] = idx
+                node_list.append(node)
+                node = node.next
+                idx += 1
+            return idx_map, node_list
+
+        if not other:
+            return False
+
+        self_map, self_nodes = build_index_map(self)
+        other_map, other_nodes = build_index_map(other)
+
+        if len(self_nodes) != len(other_nodes):
+            return False
+
+        for n1, n2 in zip(self_nodes, other_nodes):
+            if n1.val != n2.val:
+                return False
+
+            r1 = self_map.get(n1.random, None) if n1.random else None
+            r2 = other_map.get(n2.random, None) if n2.random else None
+            if r1 != r2:
+                return False
+
+        return True
