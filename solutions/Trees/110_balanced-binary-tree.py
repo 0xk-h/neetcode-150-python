0a07d5b6db8530195982bb726abf8ad055a4a796
kishore399 <kishore.j21.01@gmail.com>
2025-11-24 15:43:05 +0530

    feat: add solution for balanced binary tree problem

---
+++ solutions/Trees/110_balanced-binary-tree.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from tree import TreeNode
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return 1
+
+            left, right = dfs(root.left), dfs(root.right)
+
+            if not left or not right or abs(left - right) > 1:
+                return False
+
+            return 1 + max(left, right)
+
+        return True if dfs(root) else False
+    
+# Time Complexity: O(n)
+# Space Complexity: O(1)
