b2b5ac99aecf7120b66a8a44ba33d9343e2afc59
kishore399 <kishore.j21.01@gmail.com>
2025-11-24 12:01:17 +0530

    feat: implement iterative solution for max depth of binary tree

--- solutions/Trees/104_maximum-depth-of-binary-tree.py
+++ solutions/Trees/104_maximum-depth-of-binary-tree.py
@@ -9,3 +9,23 @@ class Solution:
     
 # Time Complexity: O(n)
 # Space Complexity: O(1)
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        stk = [(root, 1)]
+
+        while stk:
+            node, depth = stk.pop()
+
+            if node:
+                stk.append((node.right, depth + 1))
+                stk.append((node.left, depth + 1))
+            else:
+                res = max(res, depth - 1)
+
+        return res
+    
+# Time Complexity: O(n)
+# Space Complexity: O(1)
