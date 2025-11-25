24e0b50e284120741f134b9ab9832f5967448fc9
kishore399 <kishore.j21.01@gmail.com>
2025-11-25 15:31:13 +0530

    feat: add solution for Lowest Common Ancestor of a Binary Search Tree

---
+++ solutions/Trees/235_lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,31 @@
+from tree import TreeNode
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+
+        return root
+    
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        while root:
+            if p.val < root.val and q.val < root.val:
+                root = root.left
+
+            elif p.val > root.val and q.val > root.val:
+                root = root.right
+
+            else:
+                return root
+            
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
