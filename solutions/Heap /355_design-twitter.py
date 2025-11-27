08ed837780c182fa6c7ca6f222febf0f669028f0
kishore399 <kishore.j21.01@gmail.com>
2025-11-27 23:01:18 +0530

    feat: implement Twitter class without heap

--- solutions/Heap /355_design-twitter.py
+++ solutions/Heap /355_design-twitter.py
@@ -58,7 +58,80 @@ obj.unfollow(followerId,followeeId)
 #---------------------------------------------------------
 #           Time Complexity:
 #           postTweet:       O(1)
-#           getNewsFeed:     O(n)
+#           getNewsFeed:     O(n)  -> n is the total number of tweets
+#           follow:          O(1)
+#           unfollow:        O(1)
+#           
+#           Space Complexity:
+#           O(n)
+#---------------------------------------------------------
+
+
+# No Heap
+class Twitter:
+
+    def __init__(self):
+        self.follower = {}
+        self.tweet = {}
+        self.time = 0
+
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        if userId in self.tweet:
+            self.tweet[userId].append((self.time, tweetId))
+
+        else:
+            self.tweet[userId] = [(self.time, tweetId)]
+
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        res = []
+        users = []
+
+        if userId in self.tweet:
+            users.append(userId)
+
+        for user in self.follower.get(userId, []):
+            if user in self.tweet:
+                users.append(user)
+
+        idx = [len(self.tweet[user]) - 1 for user in users]
+        for _ in range(10):
+            recent = (-1, -1)
+            selected = -1
+            for i in range(len(users)):
+                index = idx[i]
+                if index != -1 and self.tweet[users[i]][index][0] > recent[0]:
+                    recent = self.tweet[users[i]][index]
+                    selected = i
+
+            if recent[0] == -1:
+                return res
+
+            res.append(recent[1])
+            idx[selected] -= 1
+
+        return res
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.follower:
+            self.follower[followerId].add(followeeId)
+
+        else:
+            self.follower[followerId] = {followeeId}
+
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.follower:
+            self.follower[followerId].remove(followeeId)
+
+
+#---------------------------------------------------------
+#           Time Complexity:
+#           postTweet:       O(1)
+#           getNewsFeed:     O(f)  -> f is the total number of followers
 #           follow:          O(1)
 #           unfollow:        O(1)
 #           
