a2c2728ac9da2f840f8c59a36ee3c84b69160657
kishore399 <kishore.j21.01@gmail.com>
2025-11-30 15:47:11 +0530

    feat: add climbing stairs solution using recursion with memoization

--- solutions/1-D Dynamic Programming/70_climbing-stairs.py
+++ solutions/1-D Dynamic Programming/70_climbing-stairs.py
@@ -24,3 +24,25 @@ class Solution:
     
 # Time Complexity: O(n)
 # Space Complexity: O(1)
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = {}
+
+        def back(i = 0):
+            if i == n:
+                return 1
+            if i > n:
+                return 0
+
+            if i in cache:
+                return cache[i]
+
+            cache[i] = back(i + 1) + back(i + 2)
+            return cache[i]
+
+        return back()
+    
+# Time Complexity: O(n)
+# Space Complexity: O(n)
