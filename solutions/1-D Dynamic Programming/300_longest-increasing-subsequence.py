9d4b6175eaf19bfca2bde4677530b41de5f4384f
kishore399 <kishore.j21.01@gmail.com>
2025-12-02 15:54:00 +0530

    feat: implement O(n log n) solution for longest increasing subsequence

--- solutions/1-D Dynamic Programming/300_longest-increasing-subsequence.py
+++ solutions/1-D Dynamic Programming/300_longest-increasing-subsequence.py
@@ -17,3 +17,28 @@ class Solution:
     
 # Time Complexity: O(n^2)
 # Space Complexity: O(n)
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [nums[0]]
+
+        for num in nums[1:]:
+            if dp[-1] < num:
+                dp.append(num)
+
+            else:
+                l, r = 0, len(dp) -1
+                while l < r:
+                    mid = l + (r-l) // 2
+
+                    if dp[mid] < num:
+                        l = mid + 1
+                    else:
+                        r = mid
+
+                dp[l] = num
+
+        return len(dp)
+    
+# Time Complexity: O(n logn)
+# Space Complexity: O(n)
