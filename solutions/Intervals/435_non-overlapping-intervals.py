7439049fea48865629644bf9b8d45f085f57caf2
kishore399 <kishore.j21.01@gmail.com>
2025-12-13 23:51:59 +0530

    feat: add solution for non-overlapping intervals

--- solutions/Intervals/435_non-overlapping-intervals.py
+++ solutions/Intervals/435_non-overlapping-intervals.py
@@ -18,3 +18,22 @@ class Solution:
     
 # Time Complexity: O(n log n)
 # Space Complexity: O(1)
+
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key = lambda x: x[1])
+        prevEnd = intervals[0][1]
+
+        res = 0
+        for currStart, currEnd in intervals[1:]:
+            if prevEnd > currStart:
+                res += 1
+
+            else:
+                prevEnd = currEnd
+
+        return res
+    
+# Time Complexity: O(n log n)
+# Space Complexity: O(1)
