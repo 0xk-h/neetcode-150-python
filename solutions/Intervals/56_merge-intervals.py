e83a8f1ccfbc4000b3b1da055c52bec978e81c1a
kishore399 <kishore.j21.01@gmail.com>
2025-12-13 23:12:10 +0530

    feat: add solution for merge intervals

---
+++ solutions/Intervals/56_merge-intervals.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        res = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+            if res[-1][1] >= intervals[i][0]:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+
+            else:
+                res.append(intervals[i])
+
+        return res
+    
+# Time Complexity: O(n log n)
+# Space Complexity: O(n)
