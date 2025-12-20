ccf84a820f8787c5bc278de1343f6d147659b8bc
kishore399 <kishore.j21.01@gmail.com>
2025-12-20 15:39:00 +0530

    feat: add solution for best time to sell stock with cooldown

---
+++ solutions/2-D Dynamic Programming/309_best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,51 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        
+        memo = [[None, None] for _ in range(n)]
+        def back(i, isCooldown, isBuyed):
+            if i >= n:
+                return 0
+
+            if isCooldown:
+                return back(i + 1, False, isBuyed)
+
+            if memo[i][isBuyed]:
+                return memo[i][isBuyed]
+                
+            res = 0
+            if isBuyed:
+                # Sell
+                res = back(i + 1, True, 0) + prices[i]
+            else:
+                # Buy
+                res = back(i + 1, False, 1) - prices[i]
+
+            res = max(res, back(i + 1, False, isBuyed))
+            memo[i][isBuyed] = max(0, res)
+            return memo[i][isBuyed]
+
+        return back(0, False, 0)
+    
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[0, 0] for _ in range(n + 2)]
+
+        for i in range(n - 1, -1, -1):
+            # have no stock (buy, hold)
+            dp[i][0] = max(dp[i + 1][1] - prices[i], dp[i + 1][0])
+
+            # have a stock (sell, hold)
+            dp[i][1] = max(dp[i + 2][0] + prices[i], dp[i + 1][1])
+
+        return dp[0][0]
+    
+# Time Complexity: O(n)
+# Space Complexity: O(n)
