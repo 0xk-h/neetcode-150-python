12d74790e693f237404e25deb032f593451c9ebc
kishore399 <kishore.j21.01@gmail.com>
2026-01-02 22:58:55 +0530

    feat: add space optimized solution for distinct subsequence

--- solutions/2-D Dynamic Programming/115_distinct-subsequences.py
+++ solutions/2-D Dynamic Programming/115_distinct-subsequences.py
@@ -46,3 +46,24 @@ class Solution:
 
 # Time Complexity: O(m * n)
 # Space Complexity: O(m * n)
+
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        dp = [0] * (n + 1)
+        dp[-1] = 1
+
+        for i in range(m - 1, -1, -1):
+            prev = 1
+            for j in range(n - 1, -1, -1):
+                temp = dp[j]
+                if s[i] == t[j]:
+                    dp[j] += prev
+
+                prev = temp
+
+        return dp[0]
+
+# Time Complexity: O(m * n)
+# Space Complexity: O(n)
