5423e12c4dfb455668ead5c73aa85a51a9c3e2de
kishore399 <kishore.j21.01@gmail.com>
2026-01-03 10:10:25 +0530

    feat: add bottom-up solution for edit distance

--- solutions/2-D Dynamic Programming/72_edit-distance.py
+++ solutions/2-D Dynamic Programming/72_edit-distance.py
@@ -23,3 +23,50 @@ class Solution:
 
 # Time Complexity: O(m * n)
 # Space Complexity: O(m * n)
+
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(m):
+            dp[i][n] = m - i
+
+        for j in range(n):
+            dp[m][j] = n - j
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1]) + 1
+
+        return dp[0][0]
+    
+# Time Complexity: O(m * n)
+# Space Complexity: O(m * n)
+
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        dp = [n - j for j in range(n + 1)]
+
+        for i in range(m - 1, -1, -1):
+            prev = dp[n]
+            dp[n] = m - i
+            for j in range(n - 1, -1, -1):
+                temp = dp[j]
+                if word1[i] == word2[j]:
+                    dp[j] = prev
+                else:
+                    dp[j] = min(dp[j], dp[j + 1], prev) + 1
+
+                prev = temp
+
+        return dp[0]
+    
+# Time Complexity: O(m * n)
+# Space Complexity: O(n)
