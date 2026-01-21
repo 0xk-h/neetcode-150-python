8b6e848ddd7e54f95a948ac2d31ae31c2525e38b
kishore399 <kishore.j21.01@gmail.com>
2026-01-21 21:51:44 +0530

    feat: add solution for encode and decode strings

--- solutions/Arrays & Hashing/271_encode-and-decode-strings.py
+++ solutions/Arrays & Hashing/271_encode-and-decode-strings.py
@@ -1,5 +1,32 @@
-#----------------------------------------------------
-#
-#          PREMIUM PROBLEM    -_-
-#
-#-----------------------------------------------------
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "-" + s
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            curr = ""
+            d = ""
+            while s[i] != "-":
+                d += s[i]
+                i += 1
+            d = int(d)
+
+            for _ in range(d):
+                i += 1
+                curr += s[i]
+
+            i += 1
+            res.append(curr)
+
+        return res
+    
+# Time Complexity: O(n)
+# Space Complexity: O(n)
