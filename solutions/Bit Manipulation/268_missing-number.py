51b357bf5f0423262c3e005c4de1e9aaf80663ec
kishore399 <kishore.j21.01@gmail.com>
2026-01-22 20:04:22 +0530

    feat: add math solution for missing numbers

--- solutions/Bit Manipulation/268_missing-number.py
+++ solutions/Bit Manipulation/268_missing-number.py
@@ -1,5 +1,18 @@
 from typing import List
 
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res += i - nums[i]
+
+        return res
+    
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
