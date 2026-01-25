ec8b96f0dc5d93116c1190d536038586afa34e97
kishore399 <kishore.j21.01@gmail.com>
2026-01-25 22:15:05 +0530

    feat: add boundary based solution for spiral matrix

--- solutions/Math & Geometry/54_spiral-matrix.py
+++ solutions/Math & Geometry/54_spiral-matrix.py
@@ -4,6 +4,43 @@ class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         total = len(matrix) * len(matrix[0])
         res = []
+        top, bottom = 0, len(matrix)
+        left, right = 0, len(matrix[0])
+        while top < bottom and left < right:
+            # Left
+            for j in range(left, right):
+                res.append(matrix[top][j])
+            top += 1
+
+            # Down
+            for i in range(top, bottom):
+                res.append(matrix[i][right - 1])
+            right -= 1
+
+            # Boundary check
+            if top >= bottom or left >= right:
+                break
+
+            # Right
+            for j in range(right -1, left -1, -1):
+                res.append(matrix[bottom - 1][j])
+            bottom -= 1
+
+            # Up
+            for i in range(bottom -1, top -1, -1):
+                res.append(matrix[i][left])
+            left += 1
+
+        return res
+    
+# Time Complexity: O(m * n)
+# Space Complexity: O(1)
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        total = len(matrix) * len(matrix[0])
+        res = []
         i, j = 0, 0
         while total > 0:
             # Right
