7e60b0b27c955f29cf824f99992632139ec3f0ac
kishore399 <kishore.j21.01@gmail.com>
2026-01-25 21:53:18 +0530

    feat: add solution for spiral matrix

---
+++ solutions/Math & Geometry/54_spiral-matrix.py
@@ -0,0 +1,89 @@
+from typing import List
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        total = len(matrix) * len(matrix[0])
+        res = []
+        i, j = 0, 0
+        while total > 0:
+            # Right
+            while j < len(matrix[i]) and matrix[i][j] != "x":
+                res.append(matrix[i][j])
+                matrix[i][j] = "x"
+                j += 1
+                total -= 1
+
+            j -= 1
+            i += 1
+
+            # Down
+            while i < len(matrix) and matrix[i][j] != "x":
+                res.append(matrix[i][j])
+                matrix[i][j] = "x"
+                i += 1
+                total -= 1
+
+            i -= 1
+            j -= 1
+
+            # Left
+            while j >= 0 and matrix[i][j] != "x":
+                res.append(matrix[i][j])
+                matrix[i][j] = "x"
+                j -= 1
+                total -= 1
+
+            j += 1
+            i -= 1
+
+            # Up
+            while i >= 0 and matrix[i][j] != "x":
+                res.append(matrix[i][j])
+                matrix[i][j] = "x"
+                i -= 1
+                total -= 1
+
+            i += 1
+            j += 1
+
+        return res
+    
+# Time Complexity: O(m * n)
+# Space Complexity: O(1)
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        while matrix:
+            res += matrix[0]
+            del matrix[0]
+
+            for i in range(len(matrix)):
+                res.append(matrix[i].pop())
+
+            while matrix and not matrix[i]:
+                matrix.pop()
+                i -= 1
+
+            if not matrix:
+                return res
+
+            for j in range(len(matrix[-1]) -1, -1, -1):
+                res.append(matrix[-1][j])
+            matrix.pop()
+
+            for i in range(len(matrix) -1, -1, -1):
+                res.append(matrix[i][0])
+                del matrix[i][0]
+
+            i = len(matrix) - 1
+            while matrix and not matrix[i]:
+                matrix.pop()
+                i -= 1
+
+        return res
+    
+# Time Complexity: O(m * n)
+# Space Complexity: O(1)
+
