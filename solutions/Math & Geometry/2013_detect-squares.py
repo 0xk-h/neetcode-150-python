ce3dc7e08615662b77e13aed21ac5513d8e216dd
kishore399 <kishore.j21.01@gmail.com>
2026-02-09 14:22:43 +0530

    feat: add solution for detect squares

---
+++ solutions/Math & Geometry/2013_detect-squares.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+from typing import List
+
+class DetectSquares:
+
+    def __init__(self):
+        self.pos = defaultdict(set)
+        self.freq = defaultdict(int)
+
+    def add(self, point: List[int]) -> None:
+        x, y = point
+        self.freq[(x, y)] += 1
+        self.pos[x].add((x, y))
+
+    def count(self, point: List[int]) -> int:
+        x1, y1 = point
+        res = 0
+        for x2, y2 in self.pos[x1]:
+            if (x2, y2) == (x1, y1):
+                continue
+
+            d = y2 - y1
+            res += self.freq[(x2, y2)] * self.freq[(x2 + d, y2)] * self.freq[(x1 + d, y1)]
+            res += self.freq[(x2, y2)] * self.freq[(x2 - d, y2)] * self.freq[(x1 - d, y1)]
+
+        return res
+
+"""
+# Your DetectSquares object will be instantiated and called as such:
+# obj = DetectSquares()
+# obj.add(point)
+# param_2 = obj.count(point)
+"""
+
+#---------------------------------------------------------
+#           Time Complexity:
+#           add:             O(1)
+#           count:           O(n) -> n is the total number of points with the same x coordinate
+#           
+#           Space Complexity:
+#           O(n) -> n is the total number of unique points
+#---------------------------------------------------------
